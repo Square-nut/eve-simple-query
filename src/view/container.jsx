0142cd48771b44d95dd2b833c0ece2d5cd19937d
liruiqing <liruiqing@300.cn>
2020-09-02 22:47:51 +0800

    feat 新增文件

---
+++ src/view/container.jsx
@@ -0,0 +1,10 @@
+import mainTree from './main-tree'
+class Container extends React.Component {
+	render() {
+		return (
+			<div>
+				<mainTree></mainTree>
+			</div>
+		)
+	}
+}
---
+++ src/view/main-tree.jsx
@@ -0,0 +1,12 @@
+class SearchTree extends React.Component {
+	constructor() {
+		super()
+	}
+	render() {
+		return
+		;<div>
+			<input type="text" className="tree-search-inp" />
+			<input type="button" value="查询" className="tree-search-btn" />
+		</div>
+	}
+}
